c97ed7fed986f4bdb046f8601e25843193830d50
ASHOK MAHARJAN <ashok997@gmail.com>
2019-05-10 16:35:17 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,7 +6,7 @@ def batch_badge_creator(array)
   badge_array= []
   array.each do |name|
     puts name
-    badge_array.push ["Hello, my name is #{name}."]
+    badge_array.push ("Hello, my name is #{name}.")
   end
   return badge_array
 end
